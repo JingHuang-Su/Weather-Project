a3a84b33ba0e6e96fa664c7a6dcb9e62ce9b55f5
JingHuang <Taiwansjh@gmail.com>
2019-09-09 23:26:53 +0800

    project deploy

--- client/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID e6751beb50f53e429cc4171f2f96a68e0b6873ecf16dc593a6c7b751e2107d2e'
-  }
-});
--- client/src/utils/weatherCondition.js
+++
@@ -1,9 +0,0 @@
-export const weatherCondition = {
-  Clear: 'fas fa-sun',
-  Thunderstorm: 'fas fa-poo-storm',
-  Drizzle: 'fas fa-cloud-sun-rain',
-  Rain: 'fas fa-cloud-rain',
-  Snow: 'far fa-snowflake',
-  Atmosphere: 'fas fa-smog',
-  Clouds: 'fas fa-cloud'
-};
